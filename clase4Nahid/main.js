42a0bf4a2558de3bee0fc8417fbe43fc37311df0
Nicolas <69121568+NicolasNahid@users.noreply.github.com>
2022-08-31 12:50:24 -0300

    Desafío 2 JS
    Simulador interactivo

---
+++ clase4Nahid/main.js
@@ -0,0 +1,13 @@
+// let cantCelulares = 0;
+// let cantTablets = 0;
+// let cantPC = 0;
+
+let cantCelulares = parseInt(prompt("Ingrese la cantidad de Celulares")) 
+let cantTablets = parseInt(prompt("Ingrese la cantidad de Tablets"))
+let cantPC = parseInt(prompt("Ingrese la cantidad de PC"))
+
+const controlStock = () => {
+    opcion = parseInt(prompt("Ingrese 1 para ver el Stock de Celulares \n Ingrese 2 para ver el Stock de Tablets \n Ingrese 3 para ver el Stock de PC"))
+    switch
+}
+
