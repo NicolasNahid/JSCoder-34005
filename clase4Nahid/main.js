aa8828608d5162033c8fc9a1010e35bf636dd473
Nicolas <69121568+NicolasNahid@users.noreply.github.com>
2022-08-31 14:34:28 -0300

    update function controlStock

--- clase4Nahid/main.js
+++ clase4Nahid/main.js
@@ -6,8 +6,29 @@ let cantCelulares = parseInt(prompt("Ingrese la cantidad de Celulares"))
 let cantTablets = parseInt(prompt("Ingrese la cantidad de Tablets"))
 let cantPC = parseInt(prompt("Ingrese la cantidad de PC"))
 
-const controlStock = () => {
-    opcion = parseInt(prompt("Ingrese 1 para ver el Stock de Celulares \n Ingrese 2 para ver el Stock de Tablets \n Ingrese 3 para ver el Stock de PC"))
-    switch
-}
+// const controlStock = () => {
+//     opcion = parseInt(prompt("Ingrese 1 para ver el Stock de Celulares \n Ingrese 2 para ver el Stock de Tablets \n Ingrese 3 para ver el Stock de PC"))
+//     switch
+// }
 
+const controlStock = (cantCelulares, cantTablets, cantPC, opcion = prompt("Ingrese 1 para ver el Stock de Celulares \n Ingrese 2 para ver el Stock de Tablets \n Ingrese 3 para ver el Stock de PC")) => {
+ switch (opcion) {
+    case "1":
+      return cantCelulares;
+      break;
+
+    case "2":
+      return cantTablets;
+      break;
+
+    case "3":
+      return cantPC;
+      break;
+
+    default:
+      return "No se encuentra en Stock";
+      break;
+ }
+} 
+
+alert(controlStock())
