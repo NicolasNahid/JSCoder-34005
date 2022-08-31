6bedcaef94f6f18a30821746da6367e29de74737
Nicolas <69121568+NicolasNahid@users.noreply.github.com>
2022-08-31 14:38:46 -0300

    fixed function controlStock
    funcion controlStock funcionando

--- clase4Nahid/main.js
+++ clase4Nahid/main.js
@@ -1,33 +1,25 @@
-// let cantCelulares = 0;
-// let cantTablets = 0;
-// let cantPC = 0;
+// let cantCelulares = parseInt(prompt("Ingrese la cantidad de Celulares")) 
+// let cantTablets = parseInt(prompt("Ingrese la cantidad de Tablets"))
+// let cantPC = parseInt(prompt("Ingrese la cantidad de PC"))
 
-let cantCelulares = parseInt(prompt("Ingrese la cantidad de Celulares")) 
-let cantTablets = parseInt(prompt("Ingrese la cantidad de Tablets"))
-let cantPC = parseInt(prompt("Ingrese la cantidad de PC"))
 
-// const controlStock = () => {
-//     opcion = parseInt(prompt("Ingrese 1 para ver el Stock de Celulares \n Ingrese 2 para ver el Stock de Tablets \n Ingrese 3 para ver el Stock de PC"))
-//     switch
-// }
-
-const controlStock = (cantCelulares, cantTablets, cantPC, opcion = prompt("Ingrese 1 para ver el Stock de Celulares \n Ingrese 2 para ver el Stock de Tablets \n Ingrese 3 para ver el Stock de PC")) => {
+const controlStock = (cantCelulares = parseInt(prompt("Ingrese la cantidad de Celulares")), cantTablets = parseInt(prompt("Ingrese la cantidad de Tablets")), cantPC = parseInt(prompt("Ingrese la cantidad de PC")), opcion = prompt("Ingrese 1 para ver el Stock de Celulares \n Ingrese 2 para ver el Stock de Tablets \n Ingrese 3 para ver el Stock de PC")) => {
  switch (opcion) {
     case "1":
-      return cantCelulares;
-      break;
+        return cantCelulares;
+    break;
 
     case "2":
-      return cantTablets;
-      break;
+        return cantTablets;
+    break;
 
     case "3":
-      return cantPC;
-      break;
+        return cantPC;
+    break;
 
     default:
-      return "No se encuentra en Stock";
-      break;
+        return "No se encuentra en Stock";
+    break;
  }
 } 
 
