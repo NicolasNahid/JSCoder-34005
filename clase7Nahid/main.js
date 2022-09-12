50c95d024381d17882f2d8cb8947ccced8c7137b
Nicolas <69121568+NicolasNahid@users.noreply.github.com>
2022-09-12 14:06:15 -0300

    fixed find function

--- clase7Nahid/main.js
+++ clase7Nahid/main.js
@@ -1,3 +1,4 @@
+const stockGeneral = [];
 class Celulares {
     constructor(id, modelo ,precio, cantidad) {
         this.id = parseInt(id);
@@ -19,13 +20,15 @@ const celularesArray = [];
 celularesArray.push(new Celulares("1", "iPhone 13", "1200", "5"));
 celularesArray.push(new Celulares("2", "iPhone 12", "1100", "3"));
 celularesArray.push(new Celulares("3", "iPhone 11", "1000", "0"));
+stockGeneral.push(new Celulares("1", "iPhone 13", "1200", "5"));
+stockGeneral.push(new Celulares("2", "iPhone 12", "1100", "3"));
+stockGeneral.push(new Celulares("3", "iPhone 11", "1000", "0"));
 
 
 
 for (const celular of celularesArray)
     celular.enStock();
 
-console.log(celularesArray);
 
 class Tablets {
     constructor(id, modelo ,precio, cantidad) {
@@ -48,13 +51,15 @@ const tabletsArray = [];
 tabletsArray.push(new Tablets("1", "Samsung A20", "500", "5"));
 tabletsArray.push(new Tablets("2", "Samsung A30", "700", "3"));
 tabletsArray.push(new Tablets("3", "Samsung A10", "400", "0"));
+stockGeneral.push(new Tablets("1", "Samsung A20", "500", "5"));
+stockGeneral.push(new Tablets("2", "Samsung A30", "700", "3"));
+stockGeneral.push(new Tablets("3", "Samsung A10", "400", "0"));
 
 
 
 for (const celular of tabletsArray)
     celular.enStock();
 
-console.log(tabletsArray);
 
 
 class Pcs {
@@ -78,13 +83,15 @@ const pcsArray = [];
 pcsArray.push(new Pcs("1", "i7", "2400", "5"));
 pcsArray.push(new Pcs("2", "i5", "1800", "3"));
 pcsArray.push(new Pcs("3", "i3", "1300", "0"));
+stockGeneral.push(new Pcs("1", "i7", "2400", "5"));
+stockGeneral.push(new Pcs("2", "i5", "1800", "3"));
+stockGeneral.push(new Pcs("3", "i3", "1300", "0"));
 
 
 
 for (const celular of pcsArray)
     celular.enStock();
 
-console.log(pcsArray);
 
 
 const controlStock = (cantCelulares, cantTablets, cantPC, opcion) => {
@@ -134,26 +141,21 @@ const controlStock = (cantCelulares, cantTablets, cantPC, opcion) => {
 
 
 
+const buscarNombre = () => {
 
-const buscadorNombre = (stockGeneral, opcion) => {
-
-    stockGeneral = [celularesArray, tabletsArray, pcsArray];
-
-    // opcion = toUpperCase.prompt("x")
-    opcion = prompt("Ingrese el nombre del/los productos a buscar")
-    let resultado = stockGeneral.filter(producto => producto.celularesArray.modelo === opcion || producto.tabletsArrayl.modelo === opcion|| producto.pcsArray.modelo === opcion)
-    alert(resultado);
-    
-
-    
+    opcion = prompt("Ingrese el nombre del producto a buscar");
+    const resultado = stockGeneral.find(item => {
+        return item.modelo == opcion
+    });
+    alert(`${JSON.stringify(resultado)}`);
+        
 }
+
+
 
 let botonControlStock = document.getElementById("boton1")
         botonControlStock.addEventListener("click", controlStock);
 
-let botonbuscadorNombre = document.getElementById("boton2")
-        botonbuscadorNombre.addEventListener("click", buscadorNombre);
+let botonBuscarNombre = document.getElementById("boton2")
+        botonBuscarNombre.addEventListener("click", buscarNombre);
 
-//dejar las arrays con 1 solo objeto
-//crear 1 solo array de productos
-//arreglar el buscador
